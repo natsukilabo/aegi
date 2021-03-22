84f94d6322b712b0523db6c84a6685635bd2255d
natsukilabo <erocostv@gmail.com>
2021-03-22 19:04:01 +0900

    修正

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const server = require('http').createServer(app);
 const request = require("request");
 var moment = require('moment');
 require('dotenv').config();
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+const url = process.env.mongodb;
 
 process.on('uncaughtException', function(err) {
 console.log(err);
@@ -25,12 +28,14 @@ var live_connection = [];
 var live_guest = {};
 var broadcast = {};
 var numClients = {};
+var usertype = {};
 
 io.sockets.on('connection', function(socket) {
 
 socket.on('live_connect',function(data,fn){
 socket.join(data.liveId);
 live_guest[socket.id] = data.liveId;
+usertype[socket.id] = "guest";
 if (numClients[data.liveId] == undefined) {
     numClients[data.liveId] = 1;
 } else {
@@ -41,6 +46,7 @@ if (numClients[data.liveId] == undefined) {
 socket.on('broadcast_connect',function(data,fn){
     socket.join(data.liveId);
     broadcast[socket.id] = data.liveId;
+    usertype[socket.id] = "broadcast";
 })
 
 socket.on('live_guest_count',function(data,fn){
@@ -60,19 +66,35 @@ socket.on("live_time_update",function(data,fn){
 })
 
 socket.on('comment', function(data,fn){
-io.emit("comment",{
-fromId : data.fromId,
-fromName : data.fromName,
-fromProfileIcon :data.fromProfileIcon,
-toId : data.toId,
-message : data.message,
-createdAt : moment().format()
-})
+    io.emit("comment",{
+        fromId : data.fromId,
+        fromName : data.fromName,
+        fromProfileIcon :data.fromProfileIcon,
+        toId : data.toId,
+        message : data.message,
+        createdAt : moment().format()
+    })
 })
 
 socket.on('disconnecting', function (data,fn) {
-    console.log(socket.room)
-    var _liveid = live_guest[socket.id];
-    numClients[_liveid]--;
+    if(usertype[socket.id] == "guest"){
+        var _liveid = live_guest[socket.id];
+        numClients[_liveid]--;
+    }else if(usertype[socket.id] == "broadcast"){
+        io.to(broadcast[socket.id]).emit("end");
+        var castId = broadcast[socket.id];
+        var liveStatus = false;
+        MongoClient.connect(url, (err, client) => {
+        assert.equal(null, err)
+        console.log('Connected successfully to server');
+        const db = client.db('aegi');
+        const date1 = new Date();
+        var doc = {
+            liveStatus:liveStatus
+        }
+        db.collection('users').updateOne({username:castId},{$set:doc},{upsert:true});
+        client.close();
+        });
+    }
 })
 })
